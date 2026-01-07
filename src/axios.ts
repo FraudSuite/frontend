1783225536d7ff7428884015f1b48015a0dd1dd0
iskandar boukhenoufa <iskandar@iskandar.iskandar@skou.com>
2026-01-07 11:39:25 -0500

    add urls

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000", // change if needed
+  baseURL: "hhttps://fraud-api-ehjw.onrender.com", // change if needed
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
